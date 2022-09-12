c8792cdd3278eaf9f6a48d9fe33a4a58f3f6f96e
xianbeil <liyichengaeqaq@gmail.com>
2022-09-12 19:25:03 +0800

    Site updated: 2022-09-12 19:25:03

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Hello aeqaq</h1>
+    <div>
+      <router-link to="/main">首页</router-link>
+      <router-link to="/content">内容页1</router-link>
+      <router-view></router-view>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Content.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>内容页1</h1>
+    
+</template>
+
+<script>
+    
+</script>
+
+<style scoped>
+
+</style>
